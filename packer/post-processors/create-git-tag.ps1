cb55d3e2ec35314288045449dddb0e13a6ad315d
Urs Voegele <unext@bluewin.ch>
2021-03-10 16:38:18 +0100

    latest sync

--- packer/post-processors/create-git-tag.ps1
+++ packer/post-processors/create-git-tag.ps1
@@ -23,7 +23,10 @@ Vagrant.configure("2") do |config|
       vb.customize ["modifyvm", :id, "--memory", "4096" ]
       vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
       vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
-      vb.customize ["modifyvm", :id, "--uartmode1", "disconnected" ]
+      vb.customize [
+        "modifyvm", :id, "--uartmode1", "file",
+        File.join(Dir.pwd, "ubuntu-bionic-18.04-cloudimg-console.log")
+      ]
       vb.customize ["modifyvm", :id, "--groups", "/#{$vb_group_name}" ]
       vb.customize ["modifyvm", :id, "--vram", 256 ]
     end
